749003214361f6cf8a4b90039030af3fa3295214
git-sreejithsreedhar <sreejithsreedhar96@gmail.com>
2025-12-09 19:10:40 +0530

    Merge feature/auth into feature/user
    - Resolve merge conflicts in http-exception.filter.ts
    - Resolve merge conflicts in user.resolver.ts
    - Resolve merge conflicts in schema.gql
    - Add new user controller from stash

---
+++ src/modules/users/presentation/controller/user.controller.ts
@@ -0,0 +1,22 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { CreateUserInput } from '../inputs/create-user.input';
+import { UserModel } from '../models/user.type';
+import { UserMapper } from '../mappers/user.mapper';
+import { CreateUserUseCase } from '../../application/use-cases/create-user.use-case';
+
+@Controller('users')
+export class UserController {
+  constructor(private readonly createUserUseCase: CreateUserUseCase) {}
+
+  @Post('signup')
+  async createUser(@Body() input: CreateUserInput): Promise<UserModel> {
+    const dto = UserMapper.toCreateUserDto(input);
+    return await this.createUserUseCase.execute(dto);
+  }
+}
--- src/modules/users/presentation/resolver/user.resolver.ts
+++ src/modules/users/presentation/resolver/user.resolver.ts
@@ -1,23 +1,23 @@
-// src/modules/users/presentation/resolvers/users.resolver.ts
-import { Resolver, Mutation, Args } from '@nestjs/graphql';
-import { UsePipes, ValidationPipe } from '@nestjs/common';
+// // src/modules/users/presentation/resolvers/users.resolver.ts
+// import { Resolver, Mutation, Args } from '@nestjs/graphql';
+// import { UsePipes, ValidationPipe } from '@nestjs/common';
 
-import { CreateUserInput } from '../inputs/create-user.input';
+// import { CreateUserInput } from '../inputs/create-user.input';
 
-import { CreateUserUseCase } from '../../application/use-cases/create-user.use-case';
-import { UserMapper } from '../mappers/user.mapper';
-import { UserModel } from '../models/user.type';
+// import { CreateUserUseCase } from '../../application/use-cases/create-user.use-case';
+// import { UserMapper } from '../mappers/user.mapper';
+// import { UserModel } from '../models/user.type';
 
-@Resolver(() => UserModel)
-export class UsersResolver {
-  constructor(private readonly createUserUseCase: CreateUserUseCase) {}
+// @Resolver(() => UserModel)
+// export class UsersResolver {
+//   constructor(private readonly createUserUseCase: CreateUserUseCase) {}
 
-  @Mutation(() => UserModel)
-  @UsePipes(new ValidationPipe({ transform: true }))
-  async createUser(@Args('input') input: CreateUserInput): Promise<UserModel> {
-    const dto = UserMapper.toCreateUserDto(input);
-    const user = await this.createUserUseCase.execute(dto);
-    // console.log(user)
-    return user;
-  }
-}
+//   @Mutation(() => UserModel)
+//   @UsePipes(new ValidationPipe({ transform: true }))
+//   async createUser(@Args('input') input: CreateUserInput): Promise<UserModel> {
+//     const dto = UserMapper.toCreateUserDto(input);
+//     const user = await this.createUserUseCase.execute(dto);
+//     // console.log(user)
+//     return user;
+//   }
+// }
