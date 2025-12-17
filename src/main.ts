ea217c1a92856bc635c3c0be4732889a5a016753
git-sreejithsreedhar <sreejithsreedhar96@gmail.com>
2025-12-17 18:40:17 +0530

    added mentor registration

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { WINSTON_MODULE_PROVIDER, WinstonModule } from 'nest-winston';
 import { Logger as WinstonLogger } from 'winston';
 import { HttpExceptionFilter } from './core/common/filters/http-exception.fillters';
-import { GqlHttpExceptionFilter } from './core/common/filters/gql-exception.filters';
-import { GlobalLoggingInterceptor } from './core/common/intercetors/global-logging.interceptor';
+// import { GqlHttpExceptionFilter } from './core/common/filters/gql-exception.filters';
+// import { GlobalLoggingInterceptor } from './core/common/intercetors/global-logging.interceptor';
 import { winstonConfig } from './core/config/logger.config';
 import cookieParser from 'cookie-parser';
 
